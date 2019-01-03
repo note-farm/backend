8e3e129b1c369ff9a436f783c436d68dec36f274
frinkfromthedrink <frinkfromthedrink@gmail.com>
2019-01-02 20:42:52 -0600

    Initial Commit

---
+++ app/controllers/category.controller.js
@@ -0,0 +1,129 @@
+const Category = require('../models/category.model.js');
+const Note = require('../models/note.model.js');
+
+// Create and Save a new category
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.description) {
+        return res.status(400).send({
+            message: "category description can not be empty"
+        });
+    }
+
+    // Create a category
+    const category = new Category({
+        title: req.body.title || "Untitled category",
+        description: req.body.description
+    });
+
+    // Save category in the database
+    category.save()
+        .then(data => {
+            res.send(data);
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "An error occurred while creating the category."
+            });
+        });
+};
+
+// Retrieve and return all categories from the database.
+exports.findAll = (req, res) => {
+    Category.find()
+        .then(categories => {
+            res.send(categories);
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving categories."
+            });
+        });
+};
+
+// Find a single category with a categoryId
+exports.findOne = (req, res) => {
+    Category.findById(req.params.categoryId)
+        .then(category => {
+            if (!category) {
+                return res.status(404).send({
+                    message: "category not found with id " + req.params.categoryId
+                });
+            }
+            res.send(category);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "category not found with id " + req.params.categoryId
+                });
+            }
+            return res.status(500).send({
+                message: "Error retrieving category with id " + req.params.categoryId
+            });
+        });
+};
+
+// Update a category identified by the categoryId in the request
+exports.update = (req, res) => {
+    // Validate Request
+    if (!req.body.description) {
+        return res.status(400).send({
+            message: "category description can not be empty"
+        });
+    }
+
+    // Find category and update it with the request body
+    Category.findByIdAndUpdate(req.params.categoryId, {
+        title: req.body.title || "Untitled category",
+        description: req.body.description
+    }, { new: true })
+        .then(category => {
+            if (!category) {
+                return res.status(404).send({
+                    message: "category not found with id " + req.params.categoryId
+                });
+            }
+            res.send(category);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "category not found with id " + req.params.categoryId
+                });
+            }
+            return res.status(500).send({
+                message: "Error updating category with id " + req.params.categoryId
+            });
+        });
+};
+
+// Delete a category with the specified categoryId in the request
+exports.delete = (req, res) => {
+    Category.findByIdAndRemove(req.params.categoryId)
+        .then(category => {
+            if (!category) {
+                return res.status(404).send({
+                    message: "category not found with id " + req.params.categoryId
+                });
+            }
+            res.send({ message: "category deleted successfully!" });
+        }).catch(err => {
+            if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+                return res.status(404).send({
+                    message: "category not found with id " + req.params.categoryId
+                });
+            }
+            return res.status(500).send({
+                message: "Could not delete category with id " + req.params.categoryId
+            });
+        });
+};
+
+// Find all notes under a specific category
+exports.findNotes = (req, res) => {
+    Note.find(req.params.category)
+        .then(notes => {
+            res.send(notes);
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Something broke"
+            });
+        });
+};
---
+++ app/routes/category.routes.js
@@ -0,0 +1,21 @@
+module.exports = (app) => {
+    const categories = require('../controllers/category.controller.js');
+
+    // Create a new Note
+    app.post('/categories', categories.create);
+
+    // Retrieve all Categories
+    app.get('/categories', categories.findAll);
+
+    // Retrieve all Notes under a Category
+    app.get('/categories/:categoryid/notes', categories.findNotes);
+
+    // Retrieve a single Note with categoryId
+    app.get('/categories/:categoryId', categories.findOne);
+
+    // Update a Note with categoryId
+    app.put('/categories/:categoryId', categories.update);
+
+    // Delete a Note with categoryId
+    app.delete('/categories/:categoryId', categories.delete);
+}
